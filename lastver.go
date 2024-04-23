68c500bc852ff689020848dcafef894182f2cc93
Achraf OUCHCHATE <135086495+aouchcha@users.noreply.github.com>
2024-04-23 20:28:46 +0100

    Update lastver.go

--- lastver.go
+++ lastver.go
@@ -10,79 +10,62 @@ import (
 
 func main() {
 
-	file := os.Args[1]
-	data, err := ioutil.ReadFile(file)
+	if len(os.Args) < 2 {
+		fmt.Println("There is an error in the commande line .")
+		return
+	}
+
+	InputFile := os.Args[1]
+
+	data, err := ioutil.ReadFile(InputFile)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	str := string(data)
-	sli := strings.Split(str, " ")
+	sli := strings.Split(str, "\n")
+	str = joinSliceOfStrings(sli)
+	sli = strings.Split(str, " ")
+
 
 	for i := 1; i < len(sli); i++ {
-		if sli[i] == "(hex)" {
-			new := ConvertBase(sli[i-1], 16)
-			sli[i-1] = new
+		if sli[0] == "(hex)" || sli[0] == "(bin)" || sli[0] == "(cap)" || sli[0] == "(low)" || sli[0] == "(up)" || sli[0] == "(cap," || sli[0] == "(low," || sli[0] == "(up," {
+			sli[0] = ""
+		} else if sli[i] == "(hex)" {
+			temp, _ := strconv.ParseInt(sli[i-1], 16, 32)
+			sli[i-1] = strconv.Itoa(int(temp))
 			sli[i] = ""
-
 		} else if sli[i] == "(bin)" {
-			new := ConvertBase(sli[i-1], 2)
-			sli[i-1] = new
+			temp, _ := strconv.ParseInt(sli[i-1], 2, 32)
+			sli[i-1] = strconv.Itoa(int(temp))
 			sli[i] = ""
-
 		} else if sli[i] == "(up)" {
-			new := ToUpper(sli[i-1])
-			sli[i-1] = new
+			sli[i-1] = strings.ToUpper(sli[i-1])
 			sli[i] = ""
-
 		} else if sli[i] == "(low)" {
-			new := ToLower(sli[i-1])
-			sli[i-1] = new
+			sli[i-1] = strings.ToLower(sli[i-1])
 			sli[i] = ""
 		} else if sli[i] == "(cap)" {
-			new := Capitalize(sli[i-1])
-			sli[i-1] = new
+			sli[i-1] = Capitalize(sli[i-1])
 			sli[i] = ""
-		} else if sli[i] == "(up," {
-			new := sli[i+1]
-			num := TrimAtoi(new)
-			sli = ToUpperNum(sli, num)
-			sli[i] = ""
-			sli[i+1] = ""
-		} else if sli[i] == "(low," {
-			new := sli[i+1]
-			num := TrimAtoi(new)
-			sli = ToLowrNum(sli, num)
-			sli[i] = ""
-			sli[i+1] = ""
-		} else if sli[i] == "(cap," {
-			new := sli[i+1]
-			num := TrimAtoi(new)
-			sli = CapNum(sli, num)
-			sli[i] = ""
-			sli[i+1] = ""
+		} else if sli[i] == "(cap," || sli[i] == "(low," || sli[i] == "(up," {
+			sli = numericalChanges(sli, i)
 		}
 	}
-	//fmt.Println(sli)
 	sli = splitWhitSpacesInSliceOfStrings(sli)
 
 	//Add whitSpaces
 	sli = addWhithSpaces(sli)
-
-	
 	sli = handelPonctuel(sli)
-		
-
 	sli = Handl_Voyel(sli)
 
+	// handel the case when the "," is in the middel of the word
 	var final_result string
 	final_result = joinSliceOfStrings(sli)
-
-	// handel the case when the "," is in the middel of the word
 	final_test := []rune(final_result)
 	var slice []rune
 	for i := 0; i < len(final_test)-1; i++ {
-		if (final_test[i] == '.' || final_test[i] == ',' || final_test[i] == '!' || final_test[i] == '?' || final_test[i] == ':' || final_test[i] == ';') && (final_test[i+1] != '.' && final_test[i+1] != ',' && final_test[i+1] != '!' && final_test[i+1] != '?' && final_test[i+1] != ':' && final_test[i+1] != ';' && final_test[i] != ' ') {
+		if (final_test[i] == '.' || final_test[i] == ',' || final_test[i] == '!' || final_test[i] == '?' || final_test[i] == ':' || final_test[i] == ';') /*&& (final_test[i+1] != '.' && final_test[i+1] != ',' && final_test[i+1] != '!' && final_test[i+1] != '?' && final_test[i+1] != ':' && final_test[i+1] != ';' && final_test[i] != ' ') */{
 			slice = append(slice, final_test[i], ' ')
 		} else {
 			slice = append(slice, final_test[i])
@@ -91,29 +74,30 @@ func main() {
 
 	//transform slice of runes into string
 	final_result = string(slice)
-
-	sli = splitWhitSpacesOfStrings(final_result)
+	sli = strings.Split(final_result, " ")
 	sli = splitWhitSpacesInSliceOfStrings(sli)
-	//fmt.Println(sli)
-	sli = Handl_singlequotes(sli)
 
-	//transform slice of strings into slice
-	final_result = joinSliceOfStrings(sli)
+	//slice = []rune{}
+	count := 0
+	slice = Handl_singlequotes(sli, &count)
 
-	//handl the case when we have the "'" in the first element
-	slice = []rune{}
-	for i := 0; i < len(final_result); i++ {
-		if final_result[i] == '\'' && final_result[i-1] == ':' {
-			slice = append(slice, ' ', rune(final_result[i]))
-		} else {
-			slice = append(slice, rune(final_result[i]))
-		}
-	}
+	// //transform slice of runes into string
+	final_result = string(slice)
+	fmt.Println(final_result)
 
 	//transform slice of runes into string
-	final_result = string(slice)
+	final_result = ""
+	for i := 0; i < len(slice)-count; i++ {
+		final_result = final_result + string(slice[i])
+	}
 
-	fmt.Println(final_result)
+	ResultFile := os.Args[2]
+	Final := []byte(final_result)
+	err = ioutil.WriteFile(ResultFile, Final, 0644)
+	if err != nil {
+		fmt.Printf("Error writing output file: %v\n", err)
+		return
+	}
 
 }
 
@@ -125,9 +109,8 @@ func splitWhitSpacesInSliceOfStrings(s []string) []string {
 			if char != ' ' {
 				word = word + string(char)
 			} else {
-				sli = append(sli, word)
+				sli = append(sli, word," ")
 				word = ""
-				continue
 			}
 		}
 		if word != "" {
@@ -135,27 +118,6 @@ func splitWhitSpacesInSliceOfStrings(s []string) []string {
 			word = ""
 		}
 	}
-	return sli
-}
-
-func splitWhitSpacesOfStrings(s string) []string {
-	var sli []string
-	word := ""
-
-	for _, char := range s {
-		if char != ' ' {
-			word = word + string(char)
-		} else {
-			sli = append(sli, word)
-			word = ""
-			continue
-		}
-	}
-	if word != "" {
-		sli = append(sli, word)
-		word = ""
-	}
-
 	return sli
 }
 
@@ -179,53 +141,6 @@ func joinSliceOfStrings(sli []string) string {
 	return word
 }
 
-func ConvertBase(s string, l int) string {
-	var base string
-	if l == 16 {
-		base = "0123456789abcdef"
-	} else {
-		base = "01"
-	}
-	result := 0
-	j := 0
-	for i := len(s) - 1; i >= 0; i-- {
-		result = result + strings.IndexByte(base, s[i])*power(l, j)
-		j++
-	}
-	new := strconv.Itoa(result)
-	return new
-}
-
-func power(x, y int) int {
-	pow := 1
-	if y > 0 {
-		pow = x * power(x, y-1)
-	}
-	return pow
-}
-
-func ToUpper(s string) string {
-	word := ""
-	for _, char := range s {
-		if char >= 'a' && char <= 'z' {
-			word = word + string(char-32)
-		} else {
-			word = word + string(char)
-		}
-	}
-	return word
-}
-func ToLower(s string) string {
-	word := ""
-	for _, char := range s {
-		if char >= 'A' && char <= 'Z' {
-			word = word + string(char+32)
-		} else {
-			word = word + string(char)
-		}
-	}
-	return word
-}
 func Capitalize(s string) string {
 	word := ""
 	for i, char := range s {
@@ -237,73 +152,25 @@ func Capitalize(s string) string {
 	}
 	return word
 }
-func ToUpperNum(sli []string, n int) []string {
-	temp_slice := []string(sli)
-	count := 0
-	j := 0
-	for i := 0; i < len(sli); i++ {
-		if sli[i] == "(up," {
-			j = i
+func numericalChanges(slice []string, nbr int) []string {
+	num := TrimAtoi(slice[nbr+1])
+	if slice[nbr] == "(up," {
+		for j := 0; j < num; j++ {
+			slice[nbr-j] = strings.ToUpper(slice[nbr-j])
 		}
-
-	}
-	for i := j - 1; i >= 0; i-- {
-		if count < n {
-			modify := ToUpper(sli[i])
-			temp_slice[i] = modify
-			count++
-		} else {
-			break
+	} else if slice[nbr] == "(low," {
+		for j := 0; j < num; j++ {
+			slice[nbr-j] = strings.ToLower(slice[nbr-j])
+		}
+	} else if slice[nbr] == "(cap," {
+		for j := 0; j < num; j++ {
+			slice[nbr-j] = Capitalize(slice[nbr-j])
 		}
 	}
-	return temp_slice
-
+	slice[nbr] = ""
+	slice[nbr+1] = ""
+	return slice
 }
-
-func ToLowrNum(sli []string, n int) []string {
-	temp_slice := []string(sli)
-	count := 0
-	j := 0
-	for i := 0; i < len(sli); i++ {
-		if sli[i] == "(low," {
-			j = i
-		}
-
-	}
-	for i := j - 1; i >= 0; i-- {
-		if count < n {
-			modify := ToLower(sli[i])
-			temp_slice[i] = modify
-			count++
-		} else {
-			break
-		}
-	}
-	return temp_slice
-
-}
-
-func CapNum(sli []string, n int) []string {
-	temp_slice := []string(sli)
-	count := 0
-	j := 0
-	for i := 0; i < len(sli); i++ {
-		if sli[i] == "(cap," {
-			j = i
-		}
-	}
-	for i := j - 1; i >= 0; i-- {
-		if count < n {
-			modify := Capitalize(sli[i])
-			temp_slice[i] = modify
-			count++
-		} else {
-			break
-		}
-	}
-	return temp_slice
-}
-
 func TrimAtoi(s string) int {
 	word := ""
 	result := 0
@@ -338,17 +205,38 @@ func handelPonctuel(sli []string) []string {
 	return sli
 }
 
-func Handl_singlequotes(finish []string) []string {
+func Handl_singlequotes(finish []string, count *int) []rune {
 	Product := []string{}
+	Product = append(Product, finish[0])
 	for i := 1; i < len(finish); i++ {
 		if finish[i] == "'" || finish[i-1] == "'" {
 			Product = append(Product, finish[i])
 		} else {
-			Product = append(Product, finish[i], " ")
+			Product = append(Product, " ", finish[i])
 		}
 	}
-	//Product = append(Product, finish[len(finish)-1])
-	return Product
+
+	final_result := joinSliceOfStrings(Product)
+	slice := []rune{}
+	for i := 0; i < len(final_result); i++ {
+		if final_result[i] == '\'' && final_result[i-1] == ':' {
+			slice = append(slice, ' ', rune(final_result[i]))
+		} else {
+			slice = append(slice, rune(final_result[i]))
+		}
+	}
+	for i := 0; i < len(slice)-1; i++ {
+		if slice[i] == ' ' && slice[i+1] == '\'' && slice[i-1] != ':' {
+			for j := i; j < len(slice)-1; j++ {
+				slice[j] = slice[j+1]
+			}
+			*count++
+		} else {
+			continue
+		}
+	}
+
+	return slice
 }
 func Handl_Voyel(sli []string) []string {
 	for i := 0; i < len(sli)-2; i++ {
